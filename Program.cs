038d912973ea83cd6c761d3ba84fb5e29dcf38eb
zitong666 <83187326+zitong666@users.noreply.github.com>
2021-05-01 16:10:14 +0200

    Hello C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zitong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.WriteLine("I am Zitong!");
+
+            Console.ReadLine();
+        }
+    }
+}
