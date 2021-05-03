fa7a4511a8b7643df75f11f8dcdb21d695211a20
zitong666 <83187326+zitong666@users.noreply.github.com>
2021-05-03 15:30:18 +0200

    data type study

---
+++ datatype.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Zitong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "C# study";
+            char grade = 'B';
+            int age = 30;
+            double gpa = 3.3;
+            bool isMale = true;// or false
+            Console.WriteLine(false);
+            Console.ReadLine();
+        }
+    }
+}
